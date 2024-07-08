bc9cd59395677208b903eeab7a629f927a14db12
Odoodevsteve <154815693+Odoodevsteve@users.noreply.github.com>
2024-07-08 18:07:06 +0530

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,14 @@
 {
     "name": "Pricelist Per Customer",
-    "summary": "",
+    "summary": "Pricelist Per Customer",
     "version": "17.0.1.0.2",
     "category": "sale",
     "author": "Edoo",
-    "website": "",
+    "website": "https://www.edooerp.com",
     "license": "AGPL-3",
     "depends": ["base","sale"],
     "installable": True,
+    'license': 'OPL-1',
+	'currency': 'EUR',
+	'price': '15',
 }
