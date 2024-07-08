d69bf6efb05036d4ebbe20db743072734e55ee09
steve <dev.erp.odoo@gmail.com>
2024-07-08 18:52:44 +0530

    [ADD]Updated code

--- pricelist_per_customer/__manifest__.py
+++ pricelist_per_customer/__manifest__.py
@@ -8,6 +8,7 @@
     "license": "AGPL-3",
     "depends": ["base","sale"],
     "installable": True,
+    "images": ['static/description/Banner.gif'],
     'license': 'OPL-1',
     'currency': 'EUR',
     'price': '15',
